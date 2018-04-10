06cb9176ad84cd84b99b8bae5e1f35c3b3baed60
lana <lana@boookeeedeMacintosh.local>
2018-04-10 18:13:07 +0800

    修改

---
+++ src/components/headnav/headnav.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="headnav" class="mheader">
+		<div class="headerwrap">
+			<div class="menuleft">
+				<el-menu :default-active="activeIndex" theme="dark" class="el-menu-demo" mode="horizontal" background-color="#545c64"
+		  text-color="#fff"
+		  active-text-color="#409EFF">
+				  <template v-for="(item,index) in navlist">
+				  	<template v-if="item.children.length === 0">
+				  		<el-menu-item :index="item.dirname"><a :href="item.link" target="_blank">{{item.name}}</a></el-menu-item>
+				  	</template>
+				  	<template v-else>
+				  		<el-submenu :index="item.dirname">
+						    <template slot="title">{{item.name}}</template>
+						    <template v-for="citem in item.children">
+						    	<el-menu-item :index="citem.dirname"><a :href="citem.link" target="_blank">{{citem.name}}</a></el-menu-item>
+						    </template>
+						</el-submenu>
+				  	</template>		  	
+				  </template>
+				</el-menu>
+		    </div>
+	    </div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'headnav',
+    props: ['activeIndex'],
+    data() {
+      return {
+        navlist: this.$t('navlist')
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    mounted(){
+    	console.log(this.navlist)
+    }
+  }
+</script>
---
+++ src/components/layshow/Layshow.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="app-wrapper" >
+		<div class="main-container">
+			<app-main></app-main>
+		</div>
+	</div>
+</template>
+<script>
+import { AppMain } from './components'
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    AppMain
+  }
+}	
+</script>
+<style>
+</style>
