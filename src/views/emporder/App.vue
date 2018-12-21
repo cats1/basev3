30f73b84e04a6410cf4e1cee484edd6c1e9cfa8c
lana <lana@boookeeedeMacintosh.local>
2018-12-21 14:26:32 +0800

    对对对

---
+++ src/views/emporder/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      activeIndex: 'index'
+    }
+  },
+  created () {},
+  mounted () {}
+}
+</script>
+<style>
+</style>
